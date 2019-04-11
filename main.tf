f41cb5366e7ff0dcd84052e2364f095e25c7ebce
Keir Badger <keir.badger@acuris.com>
2019-04-11 10:51:56 +0100

    Add env var to CNAME name

--- main.tf
+++ main.tf
@@ -66,7 +66,7 @@ resource "aws_alb_target_group" "target_group" {
 
 resource "aws_route53_record" "dns_record" {
   zone_id = "${data.aws_route53_zone.dns_domain.zone_id}"
-  name    = "${local.dns_name}.${var.dns_domain}"
+  name    = "${var.env}-${local.dns_name}.${var.dns_domain}"
 
   type            = "CNAME"
   records         = ["${var.alb_dns_name}"]
